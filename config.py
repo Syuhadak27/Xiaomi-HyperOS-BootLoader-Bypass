c14da3de948c3f26837f8aeaa4293ecd982b6088
Syuhadak27 <117827583+Syuhadak27@users.noreply.github.com>
2025-07-21 23:25:08 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+# Wajib: Token autentikasi dari Xiaomi
+COOKIE_VALUE =""
+
+# Opsional: Device ID unik (kalau kamu ingin pakai yang tetap)
+DEVICE_ID = ""  # Bisa dibiarkan kosong untuk generate otomatis
